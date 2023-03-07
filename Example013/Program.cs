708ed5ba0a269ee9fcf7aa87280807edda5c5490
Vyacheslav <slavik.spiridonow@yandex.ru>
2023-03-07 09:39:26 +0300

    сделали все три задачи

---
+++ Example013/Program.cs
@@ -0,0 +1,30 @@
+﻿//Задача 13: Напишите программу, 
+//которая выводит третью цифру заданного числа или сообщает, 
+//что третьей цифры нет.
+
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+int number = 645;
+int count = number.ToString().Length;
+Console.Write(Array(number, count));
+int Array(int a, int b)
+{
+int result = 0;
+    if (b < 3)
+    {
+        Console.WriteLine("Третьей цифры нет, ");
+    }
+    else
+    {
+        int c = 1;
+        for (int i = b; i > 3; i--)
+        {
+            c = c * 10;
+        }
+
+        result = (a / c) % 10;
+    }
+return result;
+}
---
+++ Example015/Program.cs
@@ -0,0 +1,34 @@
+﻿//Задача 15: Напишите программу, 
+//которая принимает на вход цифру, 
+//обозначающую день недели, и проверяет, является ли этот день выходным.
+
+//6 -> да
+//7 -> да
+//1 -> нет
+
+int dayNumber = ReadInt("Введите число дня недели: ");
+Console.WriteLine(weekend(dayNumber));
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+string weekend(int a)
+{
+    if (a > 0 && a < 8)
+    {
+        if (a == 7 || a == 6)
+        {
+            Console.Write("Выходной");
+        }
+        else
+        {
+            Console.Write("Рабочий");
+        }
+    }
+    else
+    {
+        Console.Write("Вы ввели число не в пределах от 1 до 7, поэтому не возможно определить");
+    }
+    return " день.";
+}
